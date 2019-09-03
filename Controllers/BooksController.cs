1bf178f12830a5d3e5d890788c5960bedd8fbaae
Programando Ideas <54816242+programando-ideas@users.noreply.github.com>
2019-09-03 17:41:11 -0500

    Books v1

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using webapi_csharp.Modelos;
+using webapi_csharp.Repositorios;
+
+namespace webapi_csharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            RPBooks rpCli = new RPBooks();
+            return Ok(rpCli.ObtenerBooks());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            RPBooks rpCli = new RPBooks();
+
+            var cliRet = rpCli.ObtenerBook(id);
+
+            if (cliRet == null)
+            {
+                var nf = NotFound("El libro " + id.ToString() + " no existe.");
+                return nf;
+            }
+
+            return Ok(cliRet);
+        }
+
+        [HttpPost("add")]
+        public IActionResult AgregarBook(Book nuevoBook)
+        {
+            RPBooks rpCli = new RPBooks();
+            rpCli.Agregar(nuevoBook);
+            return CreatedAtAction(nameof(AgregarBook), nuevoBook);
+        }
+    }
+}
---
+++ Repositorios/RPBooks.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using webapi_csharp.Modelos;
+
+namespace webapi_csharp.Repositorios
+{
+    public class RPBooks
+    {
+        public static List<Book> _listaBooks = new List<Book>()
+        {
+            new Book() { Id = 2, Title = "La hojarasca", Description = "Good one", Author = "Gabo" },
+            new Book() { Id = 3, Title = "La hojarasca", Description = "Good one", Author = "Gabo" },
+            new Book() { Id = 1, Title = "La hojarasca", Description = "Good one", Author = "Gabo" },
+        };
+
+        public IEnumerable<Book> ObtenerBooks()
+        {
+            return _listaBooks;
+        }
+
+        public Book ObtenerBook(int id)
+        {
+            var book = _listaBooks.Where(cli => cli.Id == id);
+
+            return book.FirstOrDefault();
+        }
+
+        public void Agregar(Book nuevoBook)
+        {
+            _listaBooks.Add(nuevoBook);
+        }
+    }
+}
